d968a8a6802ab588298b17b8680647d2283e34de
YingyingXie16 <yingying.xie1@gmail.com>
2020-12-28 17:16:18 -0600

    add R code, edit readme file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# pheno-changepoint
-change point estimation of phenological transitions using EVI time series from satellite imagery
+## Introduction
+Timing of life cycle events (i.e. phenology) of vegetation is an important bio-indicator of climate change, and has substantial ecological functions and impacts. Identification of phenological transitions from satellite observations still has relatively large uncertainties due to limitation of raw data time series and methodology. I developed a new method using linear change points to estimate phenological transitions for deciduous forests in the central and eastern United States. This method increases estimation accuracy for senescence date in autumn, and provides quantification of estimation uncertainty. Other than four phenological dates, the method also estimates change rates of greenup, green-down in summer, and senesence in autumn, and the EVI values on four phenological transitio dates.
+
+Here I'm sharing the R code used for change point estimation of phenological transitions using EVI time series from satellite imagery. The study is published in 2020 at the scientific journal, Remote Sensing of Environment.
+
+## Code demostration
+The code (demo_changepoint.r) is used to estimate phenological metrics using change point estimation method for 20,000 MODIS pixels sampled in the central and eastern US during 2000 and 2018. 
+
+EVI values were calculated from MODIS products: twice-daily surface reflectance data from TERRA and
+AQUA (MOD09GA and MYD09GA, Version 006), stored in netCDF files. Each file stores EVI value for all pixels in the study area at one time point.
+
+Note: Initial estimation values were provided from logistic curve fitting results based on the same EVI time series data for each pixel.
+
+### Reference
+Xie Y, Wilson AM. (2020) Change Point Estimation of Deciduous Forest Land Surface Phenology. Remote Sensing and Environment, 240. doi: 10.1016/j.rse.2020.111698 
---
+++ code/demo_changepoint.r
@@ -0,0 +1,125 @@
+#### this script:
+##        for use change point estimation for phenological transition dates in Xie&Wilson RSE 2020
+##
+
+library(segmented)
+library(foreach)
+library(parallel)
+library(doParallel)
+library(dplyr)
+library(raster)
+library(ncdf4)
+
+# set working directory
+fmosaic=""          # folder of mosaic MODIS nc files
+fevi=""             # folder of evi nc files
+fdata=""            # relevant datasets
+fseg=""             # output folder
+
+# functions----------------------------------------------------------------------
+# get twice daily netCDF files info
+filesinfo <-function(fmosaic)
+{
+  fs=data.frame(path=list.files(fmosaic,full=T,pattern=".nc"),file=list.files(fmosaic,pattern=".nc"),stringsAsFactors=F) # build file list
+  fs[,c("sensor","datecode")]=do.call("rbind",strsplit(fs$file,".",fixed=T))[,c(1,2)]
+  fs$sensordate=paste(fs$sensor,fs$datecode,sep=".")
+  fs$date=as.Date(substr(fs$file,10,16),"%Y%j")
+  fs$month=format(fs$date,"%m")
+  fs$year=format(fs$date,"%Y")
+  fs$time=ifelse(fs$sensor=="MOD09GA","10:30","13:30")
+  fs$datetime=as.POSIXct(paste(fs$date,fs$time))
+  fs$dateid=format(fs$date,"%Y%m%d")
+  return(fs)
+}
+
+# make input dataframe
+makedf=function(EVI,doyt,...){
+  data=cbind.data.frame(EVI=EVI,doyt=doyt)
+  data=na.omit(data)
+  return(data)
+}
+
+# change point estimation functions
+seg_fun <- function(data,na.rm=TRUE,...)
+{
+  #profvis({
+  #data=makedf(EVI,doyt)
+  #return(data$EVI[1:5])
+  evi.lm<-tryCatch(lm(EVI~doyt,data=data), error= function(e) return(0))    # catch error from all NAs
+  if (class(evi.lm)[1]=="numeric") {
+    return(rep(NA,23)) } else
+    {
+      #data=cbind.data.frame(EVI=EVI,doyt=doyt)
+      #data=na.omit(data)
+      spr=log_spr(data)       # logistic curve fitting for spring
+      aut=log_aut(data)       # logistic curve fitting for autumn
+      p=c(spr[5],spr[6],aut[5],aut[6])      # set logistic curve fitting results as initial estimation values
+      evi.seg<-tryCatch(segmented(evi.lm,seg.Z=~doyt,psi=p,
+                                  control=seg.control(toll = 1e-08, it.max=100, h=1, stop.if.error=T, n.boot=300)),
+                        error= function(e) return(0))      # catch error from failed segmented function
+      if (class(evi.seg)[1] == "numeric") {
+        #print(paste("Segmented estimation Fail!",sep=""))
+        return(rep(-9999,23))} else
+        {
+          #print(paste("Segmented estimation Succeed!",sep=""))
+          df=numeric(23)
+          df=c(c(evi.seg$psi[,c(2,3)]), c(intercept(evi.seg)$doyt), c(slope(evi.seg)$doyt[,c(1,2)]))
+          #varnames=c("greenup","maturity","senescence","dormancy","greenup_se","maturity_se","senescence_se","dormancy_se",
+          #           "intercept1","intercept2","intercept3","intercept4","intercept5",
+          #           "slope1","slope2","slope3","slope4","slope5","slope1_se","slope2_se","slope3_se","slope4_se","slope5_se")
+          #structure(df, names=varnames)
+          return(df)      # return a vector with 23 numbers
+        }
+    }
+  # })
+}
+
+# run code --------------------------------------------------------------------------
+# read location information for 20000 pixels
+grd=read.table(file=paste(fdata,"/loc_sample20000.csv",sep=""),header=T,sep= ",",stringsAsFactors=FALSE)
+
+# read evi nc files (one nc file stores evi values of all pixels at one time point)
+fs2=filesinfo(fmosaic)        # get files info
+fs2=fs2[order(fs2$datetime),]
+
+filenames=paste(fevi,"/",fs2$sensordate,"_evi_QAls.nc",sep="")
+d=stack(x=c(filenames))            # read all files
+
+# output 23 variable names
+varnames=c("greenup","maturity","senescence","dormancy","greenup_se","maturity_se","senescence_se","dormancy_se",
+           "intercept1","intercept2","intercept3","intercept4","intercept5",
+           "slope1","slope2","slope3","slope4","slope5","slope1_se","slope2_se","slope3_se","slope4_se","slope5_se")
+
+year=2000:2019
+
+grd=modloc_grdob   # for ground observation sites
+for (j in 1:19)
+{
+  # get time series of day and time including current year and January in next year
+  datetime=c(fs2[which(fs2$year==year[j]),10], fs2[which(fs2$year==year[j+1] & fs2$month=="01"),10])
+  if (j>1) doyt=as.numeric(datetime-as.POSIXct(paste(year[j],"-01-01",sep="")))/24 else
+    doyt=as.numeric(datetime-as.POSIXct(paste(year[j],"-01-01",sep="")))
+  bands=c(which(fs2$year==year[j]),which(fs2$year==year[j+1] & fs2$month=="01"))
+
+  # extract evi for 20000 pixels
+  df=extract(d,grd$cellid,df=T,layer=bands[1],n=length(bands))
+
+  # parallel computation
+  registerDoParallel(30)
+  getDoParWorkers()
+
+  # change point estimation
+  seg <- foreach(i=1:length(grd$cellid), .combine='rbind.data.frame') %dopar%
+    {
+      EVI=as.numeric(df[i,-1])
+      data=makedf(EVI,doyt)
+      seg_fun(data)
+    }
+
+  # assign variable names
+  colnames(seg)=varnames
+
+  # save output for each year
+  write.table(seg,file=paste(fseg,"/seg_",year[j],"_smp20000.csv",sep=""),row.names = F, sep=",")
+}
+
